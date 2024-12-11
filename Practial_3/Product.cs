a7e265383421ddf4fb67c0a08d6ce76525e8e206
Abragamchik <126409224+Abragamchik@users.noreply.github.com>
2024-12-11 12:33:21 +0200

    Add files via upload

---
+++ Practial_3/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Practial_3
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ Practial_3/Program.cs
@@ -0,0 +1,32 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Product API",
+        Version = "v1",
+        Description = "API äëÿ óïðàâë³ííÿ êàòàëîãîì òîâàð³â"
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
